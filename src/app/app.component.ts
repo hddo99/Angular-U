0e14cc96e5eb99c90d8b9e5e02090a0ade3cf00b
hddo99 <jackdo99.work@gmail.com>
2020-12-16 19:51:28 -0500

    publish generatepw-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'generatePW';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+  length = 0;
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  generateRandom(str: string) {
+    const idx = Math.floor(Math.random() * str.length)
+    return str[idx];
+  }
+  onButtonClick() {
+    const numbers = '1234567890'
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChar = '';
+    if(this.includeLetters){
+      validChar += letters;
+    }
+    if(this.includeNumbers){
+      validChar += numbers;
+    }
+    if(this.includeSymbols){
+      validChar += this.generateRandom(symbols);
+    }
+    let generatedPassword = '';
+    for(let i = 0; i < this.length; i++){
+      generatedPassword += this.generateRandom(validChar);
+    }
+    
+    this.password = generatedPassword;
+  }
+
 }
