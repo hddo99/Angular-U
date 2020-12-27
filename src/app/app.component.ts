b747a15fccdca56c3586e1aa4b905144ee96d023
hddo99 <jackdo99.work@gmail.com>
2020-12-27 04:36:00 -0500

    Typing challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'typingapp';
+  randomText = lorem.sentence();
+  enteredText: string = '';
+
+
+  onInput(event: any){
+    this.enteredText = event.target.value;
+
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if(!enteredLetter) {
+      return 'pending';
+    }
+    
+    return randomLetter === enteredLetter ? 'correct': 'incorrect';
+  }
+
+  generateNewText(){
+    this.randomText = lorem.sentence();
+    this.enteredText='';
+  }
 }
